073af13e8b01a5e36d96d01fb8325c79b6806a3a
Kevin Ahn <89061493+maketheworldwise@users.noreply.github.com>
2022-02-21 15:25:04 +0900

    Time: 0 ms (100.00%), Space: 44 MB (6.37%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (target < nums[0]) {
+          return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+          return nums.length;
+        }
+        
+        // binary search
+        int left = 0, right = nums.length - 1, mid;
+        
+        while (left < right) {
+            
+            mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+}
